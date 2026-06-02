d4718df3ccf4d694c68342c1fe84c4eeaebd16ab
Berrag <berragrizlene1977@gmail.com>
2026-06-02 13:27:23 +0200

    Fix static preview paths

--- export-docs.php
+++ export-docs.php
@@ -21,22 +21,29 @@ foreach ($pages as $url => $path) {
         continue;
     }
 
-    // Assets CSS / JS / images / vidéos
+    // Corrige les chemins assets pour GitHub Pages
     $html = str_replace($base . '/assets/', $repoBase . '/assets/', $html);
-    $html = str_replace('/assets/', $repoBase . '/assets/', $html);
+    $html = str_replace($base . '/storage/', $repoBase . '/storage/', $html);
 
-    // Liens internes
-    $html = str_replace($base . '/le-club', $repoBase . '/le-club/', $html);
-    $html = str_replace($base . '/cours-horaires', $repoBase . '/cours-horaires/', $html);
-    $html = str_replace($base . '/tarifs', $repoBase . '/tarifs/', $html);
-    $html = str_replace($base . '/contact', $repoBase . '/contact/', $html);
-    $html = str_replace($base . '/', $repoBase . '/', $html);
+    // Corrige les chemins absolus Laravel
+    $html = str_replace('href="/assets/', 'href="' . $repoBase . '/assets/', $html);
+    $html = str_replace('src="/assets/', 'src="' . $repoBase . '/assets/', $html);
 
+    $html = str_replace('href="/storage/', 'href="' . $repoBase . '/storage/', $html);
+    $html = str_replace('src="/storage/', 'src="' . $repoBase . '/storage/', $html);
+
+    // Corrige les data-base utilisés par certains scripts/images
+    $html = str_replace('data-base="/assets/', 'data-base="' . $repoBase . '/assets/', $html);
+
+    // Corrige les liens internes
     $html = str_replace('href="/le-club"', 'href="' . $repoBase . '/le-club/"', $html);
     $html = str_replace('href="/cours-horaires"', 'href="' . $repoBase . '/cours-horaires/"', $html);
     $html = str_replace('href="/tarifs"', 'href="' . $repoBase . '/tarifs/"', $html);
     $html = str_replace('href="/contact"', 'href="' . $repoBase . '/contact/"', $html);
     $html = str_replace('href="/"', 'href="' . $repoBase . '/"', $html);
+
+    // Sécurité : enlève le doublon si jamais il apparaît
+    $html = str_replace('/boxe-club/boxe-club/', '/boxe-club/', $html);
 
     $fullPath = __DIR__ . '/' . $path;
     @mkdir(dirname($fullPath), 0777, true);
